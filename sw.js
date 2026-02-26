f20aa88756c45f303551d2c93c4d8c93e06ad695
meldestellebs-create <meldestellebs@gmail.com>
2026-02-26 10:53:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,22 @@
-const CACHE_NAME = "meldestelle-bs-cache-v1";
-const OFFLINE_URLS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest"
+const CACHE_NAME = "bildungswege-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/icon-192.png",
+  "/icon-512.png"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(OFFLINE_URLS);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return (
-        response ||
-        fetch(event.request).catch(() => {
-          return caches.match("./index.html");
-        })
-      );
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
