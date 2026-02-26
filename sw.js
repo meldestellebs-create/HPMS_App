49b25a90211e96af8dd190347619bbfb723b96e2
meldestellebs-create <meldestellebs@gmail.com>
2026-02-26 11:47:40 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,45 @@
 const CACHE_NAME = "bildungswege-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.webmanifest",
-  "/icon-192.png",
-  "/icon-512.png"
+  "./",
+  "./index.html", 
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
+// Install
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Activate  
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request).catch(() => {
+          return cache.match('./index.html');
+        });
+      });
+    })
   );
 });
